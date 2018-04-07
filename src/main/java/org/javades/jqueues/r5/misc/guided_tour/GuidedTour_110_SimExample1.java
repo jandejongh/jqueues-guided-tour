0647fc6d28165d41920bc2384cc8f453aa1d304a
Jan de Jongh <jfcmdejongh@gmail.com>
2018-04-07 21:07:00 +0200

    Added javadoc [for one, to satisfy maven].

--- src/main/java/org/javades/jqueues/r5/misc/guided_tour/GuidedTour_110_SimExample1.java
+++ src/main/java/org/javades/jqueues/r5/misc/guided_tour/GuidedTour_110_SimExample1.java
@@ -9,9 +9,29 @@ import org.javades.jqueues.r5.listener.StdOutSimEntityListener;
 import org.javades.jsimulation.r5.DefaultSimEventList;
 import org.javades.jsimulation.r5.SimEventList;
 
-final class GuidedTour_110_SimExample1
+/** Example from the "Hello World" chapter.
+ * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180407, r5.2.0.
+ * 
+ * @author Jan de Jongh, TNO
+ * 
+ */
+public final class GuidedTour_110_SimExample1
 {
   
+  /** Prevents instantiation.
+   * 
+   */
+  private GuidedTour_110_SimExample1 ()
+  {
+  }
+  
+  /** Runs the example.
+   * 
+   * @param args The command-line arguments (ignored).
+   * 
+   */
   public static void main (final String[] args)
   {
     final SimEventList el = new DefaultSimEventList (0);
